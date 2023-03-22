0d8dc058e80bdcb55b4ba7544a2f3bc94fbff4a8
Marina Shnyakina <schnyakina@yandex.ru>
2023-03-22 18:33:57 +0300

    Connected Firebase, added the main page and the tegistration page

--- app/src/main/java/com/example/yourway_petproject/MainActivity.java
+++ app/src/main/java/com/example/yourway_petproject/MainActivity.java
@@ -1,14 +1,132 @@
 package com.example.yourway_petproject;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+import com.example.yourway_petproject.Models.User;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.rengwuxian.materialedittext.MaterialEditText;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnCome, btnRegister;
+    EditText name, phone, email, password;
+    FirebaseAuth auth;
+    FirebaseDatabase db;
+    DatabaseReference users;
+    RelativeLayout root;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnCome = findViewById(R.id.btnCome);
+        btnRegister = findViewById(R.id.btnRegister);
+
+        root = findViewById(R.id.root_element);
+
+        auth = FirebaseAuth.getInstance();
+        db = FirebaseDatabase.getInstance();
+        users = db.getReference("Users");
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showRegisterWindow();
+            }
+        });
     }
+
+    private void showRegisterWindow() {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("Зарегистрироваться");
+        dialog.setMessage("Введите все данные для регистрации");
+
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View registerWindow = inflater.inflate(R.layout.register_window, null);
+        dialog.setView(registerWindow);
+
+        name = registerWindow.findViewById(R.id.editTextPersonName);
+        phone = registerWindow.findViewById(R.id.editTextPhone);
+        email = registerWindow.findViewById(R.id.editTextEmailAddress);
+        password = registerWindow.findViewById(R.id.editTextPassword);
+
+        dialog.setNegativeButton("Отменить", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int which) {
+                dialogInterface.dismiss();
+            }
+        });
+
+        dialog.setPositiveButton("Добавить", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int which) {
+                if (TextUtils.isEmpty(email.getText().toString())) {
+                    // возможно вместо этого нужно будет написать
+                    // Snackbar.make(root, "сообщение", Snackbar.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "Введите вашу почту", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(name.getText().toString())) {
+                    Toast.makeText(MainActivity.this, "Введите ваше имя", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(phone.getText().toString())) {
+                    Toast.makeText(MainActivity.this, "Введите ваш телефон", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (password.getText().toString().length() < 7) {
+                    Toast.makeText(MainActivity.this, "Введите ваш пароль, который более 7 символов", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                // Регистрация пользователя
+                auth.createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString())
+                        .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                            @Override
+                            public void onSuccess(AuthResult authResult) {
+                                User user = new User();
+                                user.setEmail(email.getText().toString());
+                                user.setName(name.getText().toString());
+                                user.setPassword(password.getText().toString());
+                                user.setPhone(phone.getText().toString());
+
+                                users.child(user.getEmail())
+                                        .setValue(user)
+                                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                            @Override
+                                            public void onSuccess(Void unused) {
+                                                Toast.makeText(MainActivity.this, "Пользователь добавлен!", Toast.LENGTH_SHORT).show();
+                                            }
+                                        });
+
+
+                            }
+                        });
+
+            }
+        });
+
+        dialog.show();
+
+    }
+
 }
